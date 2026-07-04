9a0b7525d1f5ab803ba8539a874115181aee2c67
SimbiOS/Matverse <luamatverse@gmail.com>
2026-07-04 17:09:36 -0300

    feat(urano): export AuditoriaPR public API

---
+++ urano/auditoria_pr/__init__.py
@@ -0,0 +1,27 @@
+from .core import (
+    AspectInference,
+    AspectLexicon,
+    DerivedMention,
+    EpistemicClass,
+    ExplicitAspectSentimentExtractor,
+    GateDecision,
+    GateResult,
+    KairosUranoPipeline,
+    OmegaGate,
+    ProductHypothesis,
+    RawObservation,
+)
+
+__all__ = [
+    "AspectInference",
+    "AspectLexicon",
+    "DerivedMention",
+    "EpistemicClass",
+    "ExplicitAspectSentimentExtractor",
+    "GateDecision",
+    "GateResult",
+    "KairosUranoPipeline",
+    "OmegaGate",
+    "ProductHypothesis",
+    "RawObservation",
+]
